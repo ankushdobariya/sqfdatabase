a915fcffc3431a8d39370db45b6a2709e57a78c1
ankushdobariya <106047120+ankushdobariya@users.noreply.github.com>
2022-11-17 11:06:46 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sqfdatabase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqfdatabase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:sqfdatabase/view_page.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: view_page(),));
+
+}
